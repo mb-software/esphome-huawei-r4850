11d8823d9d1cee975871b6b8e0b935137b145dc7
patagona <patagonahn@gmail.com>
2025-02-19 18:48:57 +0100

    add standby switch

--- components/huawei_r4850/switch/__init__.py
+++ components/huawei_r4850/switch/__init__.py
@@ -9,6 +9,7 @@ from .. import HuaweiR4850Component, huawei_r4850_ns, CONF_HUAWEI_R4850_ID
 
 
 CONF_FAN_SPEED_MAX = "fan_speed_max"
+CONF_STANDBY = "standby"
 
 HuaweiR4850Switch = huawei_r4850_ns.class_(
     "HuaweiR4850Switch", switch.Switch, cg.Component
@@ -19,6 +20,11 @@ CONFIG_SCHEMA = cv.All(
         {
             cv.GenerateID(CONF_HUAWEI_R4850_ID): cv.use_id(HuaweiR4850Component),
             cv.Optional(CONF_FAN_SPEED_MAX): switch.SWITCH_SCHEMA.extend(
+                {
+                    cv.GenerateID(): cv.declare_id(HuaweiR4850Switch)
+                }
+            ),
+            cv.Optional(CONF_STANDBY): switch.SWITCH_SCHEMA.extend(
                 {
                     cv.GenerateID(): cv.declare_id(HuaweiR4850Switch)
                 }
@@ -37,3 +43,10 @@ async def to_code(config):
         await switch.register_switch(var, conf)
         cg.add(getattr(hub, "register_input")(var))
         cg.add(var.set_parent(hub, 0x34))
+    if config[CONF_STANDBY]:
+        conf = config[CONF_STANDBY]
+        var = cg.new_Pvariable(conf[CONF_ID])
+        await cg.register_component(var, conf)
+        await switch.register_switch(var, conf)
+        cg.add(getattr(hub, "register_input")(var))
+        cg.add(var.set_parent(hub, 0x32))
--- components/huawei_r4850/switch/huawei_r4850_switch.cpp
+++ components/huawei_r4850/switch/huawei_r4850_switch.cpp
@@ -5,6 +5,7 @@ namespace esphome {
 namespace huawei_r4850 {
 
 static const int8_t SET_FAN_SPEED_MAX_FUNCTION = 0x34;
+static const int8_t SET_STANDBY_FUNCTION = 0x32;
 
 void HuaweiR4850Switch::write_state(bool state) {
   parent_->set_value(this->functionCode_, state, 0);
@@ -23,6 +24,7 @@ void HuaweiR4850Switch::handle_update(bool success, uint8_t function_code, bool 
 void HuaweiR4850Switch::set_offline() {
   switch (this->functionCode_) {
     case SET_FAN_SPEED_MAX_FUNCTION: // fan speed doesnt have offline
+    case SET_STANDBY_FUNCTION: // don't save standby mode (if that is even possible)
     default:
       break;
   }
